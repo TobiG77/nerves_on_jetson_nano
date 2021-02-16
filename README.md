015b8cc03fa8fcc2506ae9d15c3362b34b15779c
Tobias Gerschner <tobias.gerschner@gmail.com>
2021-02-16 16:51:29 +1300

    compiles, but does not boot

--- README.md
+++ README.md
@@ -14,6 +14,26 @@ information about targets see:
 
 https://hexdocs.pm/nerves/targets.html#content
 
+
+## Build Firmware
+
+In order to build the firmware you need to have additional repositories present in a similar folder structure:
+
+/parent-directory/
+  - your_nerves_project
+  - nerves_system_jetson_nano # from https://github.com/verypossible/nerves_system_jetson_nano
+
+The nerves_system_jetson_nano requires additional sources
+- linux-tegra-4.9 # from https://github.com/OE4T/linux-tegra-4.9
+
+The nerves_system_jetson_nano expects to use the tegra patched kernel source. I am not sure what the expected method of integration is, but this works:
+
+```bash
+tar -czv --exclude="*.git" -f ~/.nerves/dl/linux/linux-oe4t-patches-l4t-r32.5.tar.gz linux-tegra-4.9
+```
+
+After this you can run the usual steps as outlined in `Getting Started`
+
 ## Getting Started
 
 To start your Nerves app:
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule NervesOnJetsonNano.MixProject do
 
   @app :nerves_on_jetson_nano
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64]
+  @all_targets [:jetson_nano]
 
   def project do
     [
@@ -41,15 +41,7 @@ defmodule NervesOnJetsonNano.MixProject do
       {:nerves_pack, "~> 0.4.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.13", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.13", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.13", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.13", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.13", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.13", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.8", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.4", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.13", runtime: false, targets: :x86_64}
+      {:nerves_system_jetson_nano, path: "../nerves_system_jetson_nano", nerves: [compile: true]}
     ]
   end
 
